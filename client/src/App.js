f13bd17dc10c73c90faf886e759f837196a2c712
yahiahwewi <74486724+yahiahwewi@users.noreply.github.com>
2022-12-28 12:23:16 +0100

    Fix layout issues & api

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,38 @@
+import { useEffect, useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const[todos,setTodos]=useState([]);
+	const [popupActive, setPopupActive]=useState(false);
+	const[newTodo,setNewTodo]=useState("");
+
+
+	return (
+<div className="App">
+<h1>Welcome , Yahya</h1>
+<h4>Your tasks :</h4>
+<div className='todos'> 
+	<div className='todo'> 
+		<div className='checkbox'>  </div>
+		<div className='text'>Get the bread</div>
+		<div className="delete-todo"></div>
+
+</div>
+		
+	<div className='todo'> 
+		<div className='checkbox'>  </div>
+		<div className='text'>Get the Milk</div>
+		<div className="delete-todo"></div>
+
+
+</div>
+</div>
+</div>
+
+
+	);
 }
 
 export default App;
